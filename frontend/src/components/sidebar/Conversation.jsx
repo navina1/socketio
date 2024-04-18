296a975a4e1003f43fe9c84080429a78f475d4d2
Navina K <navinak321@gmail.com>
2024-04-18 11:55:35 +0530

    conversations part completed

--- frontend/src/components/sidebar/Conversation.jsx
+++ frontend/src/components/sidebar/Conversation.jsx
@@ -1,18 +1,19 @@
 import React from 'react'
 
-function Conversation() {
+function Conversation({key,conversation,emoji}) {
+    console.log(key)
     return (
         <>
             <div className='flex gap-2 items-center hover:bg-sky-500 rounded p-2 py-1 cursor-pointer '>
                 <div className="avatar online">
                     <div className="w-12 rounded-full">
-                        <img src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+                        <img src={conversation.profilePicture} />
                     </div>
                 </div>
                 <div className='flex flex-col flex-1'>
                     <div className='flex gap-3 justify-between'>
-                        <p className='font-bold text-gray-200'>Anu</p>
-                        <span className='text-xl'>🍀</span>
+                        <p className='font-bold text-gray-200'>{conversation.fullName}</p>
+                        <span className='text-xl'>{emoji}</span>
                     </div>
                 </div>
             </div>
--- frontend/src/components/sidebar/Conversations.jsx
+++ frontend/src/components/sidebar/Conversations.jsx
@@ -1,15 +1,22 @@
 import React from 'react'
 import Conversation from './Conversation'
+import useGetConversation from '../../hooks/useGetConversation'
+import { getRandomEmoji } from '../../utils/emojis';
 
 function Conversations() {
+  const {loading,conversations}=useGetConversation();
+  console.log("conversations", conversations);
   return (
     <div className='py-2 flex flex-col overflow-auto'>
-        <Conversation/>
-        <Conversation/>
-        <Conversation/>
-        <Conversation/>
-        <Conversation/>
-        <Conversation/>
+      
+       {conversations?.map((conversation)=>(
+          <Conversation
+              key={conversation._id}
+              conversation={conversation}
+              emoji={getRandomEmoji()}
+          />
+       ))}
+        {loading ? <span className='loading loading-spinner'></span>:null}
     </div>
   )
 }
